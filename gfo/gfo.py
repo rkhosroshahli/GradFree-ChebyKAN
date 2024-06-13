c1e30e4f77f858d5b158026739475a77708a73eb
rkhosrowshahli <rasa.khosrowshahli@ontariotechu.net>
2024-06-13 17:44:57 -0400

    improve GFO and add smaller networks

--- gfo/gfo.py
+++ gfo/gfo.py
@@ -1,21 +1,26 @@
+import itertools
+import sys
+from time import sleep
+import time
 import torch
 from torch.utils.data import DataLoader, Subset
 
 import numpy as np
 import pandas as pd
+
 from sklearn.metrics import (
     f1_score,
     top_k_accuracy_score,
 )
+
+from torcheval.metrics.functional import multiclass_f1_score, topk_multilabel_accuracy
 import matplotlib.pyplot as plt
 
 
 from pymoo.core.problem import Problem
 from pymoo.core.callback import Callback
 
-import jax
-import jax.numpy as jnp
-from jax import vmap, jit
+import multiprocessing as mp
 
 
 class GFOProblem(Problem):
@@ -23,8 +28,8 @@ class GFOProblem(Problem):
         self,
         n_var=None,
         model=None,
-        sample_size=None,
         dataset=None,
+        batch_size=1024,
         block=False,
         codebook=None,
         orig_dims=None,
@@ -43,7 +48,7 @@ class GFOProblem(Problem):
             vtype=float,
         )
         self.model = model
-        self.sample_size = sample_size
+        self.batch_size = batch_size
         self.block = block  # Enable / disable block
         self.dataset = dataset
         self.test_loader = test_loader
@@ -67,100 +72,122 @@ class GFOProblem(Problem):
             self.data_loader = train_loader
 
     def data_sampler(self):
-        # random_indices = np.random.uniform(low=0, high=len(self.dataset), size=self.sample_size).astype(int)
-        random_indices = np.random.choice(
-            np.arange(len(self.dataset)), size=self.sample_size, replace=False
-        )
-        random_dataset = Subset(self.dataset, random_indices)
-        return DataLoader(random_dataset, batch_size=128, shuffle=True)
+        return DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True)
 
     def unblocker(self, blocked_params):
 
         unblocked_params = np.ones(self.orig_dims)
+        # start_time = time.time()
         for block_idx, indices in self.codebook.items():
             unblocked_params[indices] *= blocked_params[block_idx]
 
+        # end_time = time.time() - start_time
+
+        # print(end_time)
         return unblocked_params
 
-    def unblocker_jax(self, blocked_params):
+    def assign_values(self, args):
+        b = np.zeros(self.orig_dims)
+        a, indices = args[0], args[1]
 
-        # # Initialize B with zeros
-        # B = jnp.zeros(10)
+        b[indices] = a
+        # b[0] = indices[0]
+        return b
 
-        # # Define a function to update B based on A and codebook
-        # def update_B(b, a, indices):
-        #     b = b.at[indices].set(a)
-        #     return b
+    def unblocker_multiprocess(self, blocked_params):
+        flattened_indices = []
+        for i, indices in self.codebook.items():
+            flattened_indices.extend(indices)
 
-        # # Vectorize the function using vmap
-        # vectorized_update_B = vmap(update_B, in_axes=(None, 0, 0))
+        unblocked_params = np.zeros(self.orig_dims)
+        pool = mp.Pool(processes=mp.cpu_count() // 2)
+        result = pool.map(
+            self.assign_values,
+            zip(blocked_params, self.codebook.values()),
+        )
 
-        # # Prepare the indices and values for vmap
-        # indices = [self.codebook[i] for i in range(len(blocked_params))]
-        # values = blocked_params
+        unblocked_params = np.sum(result, 0)
 
-        # # Use jit to compile the function
-        # compiled_update_B = jit(vectorized_update_B)
-        # Define the indices to be updated
-        # Function to distribute A values into B according to the codebook
-        # B = jnp.zeros(self.orig_dims)
+        return unblocked_params
 
-        def copy_values(A, codebook):
-            B = jnp.zeros(self.orig_dims)  # Initialize B with zeros
-            for key, indices in codebook.items():
-                B = B.at[indices].set(A)
-            return B
+    # def update_B(self, indices, value):
+    #     B = jnp.zeros(self.orig_dims)
+    #     B = B.at[indices].set(value)
+    #     return B
 
-        # Use vmap to efficiently apply the function across multiple copies of A
-        parallel_copy = vmap(lambda x: copy_values(x, self.codebook))
+    # def unblocker_jax_failed(self, A):
+    #     # Flatten the codebook to a list of indices and corresponding values
+    #     flattened_indices = []
+    #     flattened_values = []
 
-        # Apply the vectorized function
-        unblocked_params = parallel_copy(blocked_params)
-        unblocked_params_np = jax.device_get(unblocked_params)
-        print(unblocked_params_np.shape)
+    #     for i, indices in self.codebook.items():
+    #         flattened_indices.extend(indices)
+    #         flattened_values.extend([A[i]] * len(indices))
 
-        return unblocked_params_np
+    #     flattened_indices = jnp.array(flattened_indices)
+    #     flattened_values = jnp.array(flattened_values)
+    #     # Use vmap to efficiently apply the function across multiple copies of A
+    #     # Use vmap to vectorize the process of updating B
+    #     updated_Bs = jax.vmap(self.update_B, in_axes=(0, 0))(
+    #         flattened_indices[:, None], flattened_values[:, None]
+    #     )
+
+    #     print(updated_Bs)
+
+    #     # Sum the results to get the final B vector
+    #     final_B = jnp.sum(updated_Bs, axis=0)
+
+    #     # Convert the final B to a numpy array
+    #     final_B_np = np.array(final_B)
+
+    #     return final_B_np
 
     def crossentropy_func(self, model, data_loader, device):
         model.eval()
         fitness = 0
 
-        for idx, (data, target) in enumerate(data_loader):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            fitness += torch.nn.functional.cross_entropy(output, target).item()
+        data, target = next(iter(data_loader))
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        fitness += torch.nn.functional.cross_entropy(output, target).item()
 
-        return fitness / len(data_loader)
+        return fitness
 
     def f1score_func(self, model, data_loader, device):
         model.eval()
         fitness = 0
 
-        for idx, (data, target) in enumerate(data_loader):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            pred = output.argmax(dim=1, keepdim=False)
+        # for idx, (data, target) in enumerate(data_loader):
+        data, target = next(iter(data_loader))
+        data, target = data.to(device), target.to(device)
+        output = model(data)
 
-            fitness += f1_score(
-                y_true=target.cpu().numpy(), y_pred=pred.cpu().numpy(), average="macro"
-            )
+        # fitness += f1_score(
+        #     y_true=target.cpu().numpy(), y_pred=pred.cpu().numpy(), average="macro"
+        # )
+        fitness += multiclass_f1_score(
+            output, target, average="macro", num_classes=10
+        ).item()
 
-        return 1 - (fitness / len(data_loader))
+        return 1 - fitness
 
     def top1_func(self, model, data_loader, device):
         model.eval()
         fitness = 0
 
-        for idx, (data, target) in enumerate(data_loader):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            pred = output.argmax(dim=1, keepdim=False)
+        data, target = next(iter(data_loader))
+        data, target = data.to(device), target.to(device)
+        output = model(data)
 
-            fitness += top_k_accuracy_score(
-                y_true=target.cpu().numpy(), y_pred=pred.cpu().numpy(), k=1
-            )
+        fitness += top_k_accuracy_score(
+            y_true=target.cpu().numpy(),
+            y_score=output.cpu().detach().numpy(),
+            k=1,
+            labels=np.arange(10),
+        )
+        # fitness += topk_multilabel_accuracy(output, target, k=5).item()
 
-        return 1 - (fitness / len(data_loader))
+        return 1 - fitness
 
     def test_func(self, X):
         uxi = X.copy()
@@ -177,11 +204,9 @@ class GFOProblem(Problem):
                 output = self.model(data)
                 pred = output.argmax(dim=1)
 
-                fitness += f1_score(
-                    y_true=target.cpu().numpy(),
-                    y_pred=pred.cpu().numpy(),
-                    average="macro",
-                )
+                fitness += multiclass_f1_score(
+                    pred, target, average="macro", num_classes=10
+                ).item()
 
         return fitness / len(self.test_loader)
 
@@ -191,7 +216,7 @@ class GFOProblem(Problem):
     def _calc_pareto_set(self):
         return np.full(self.n_var, 0)
 
-    def _evaluate(self, X, out, reset=True, *args, **kwargs):
+    def _evaluate(self, X, out, *args, **kwargs):
         NP = X.shape[0]
         fout = np.zeros(NP)
 
@@ -237,22 +262,36 @@ class SOCallback(Callback):
         self.data["opt_F"] = []
         self.data["pop_F"] = []
         self.data["n_evals"] = []
+        self.start_iter = None
+        self.start_eval = None
 
     def notify(self, algorithm):
         self.data["opt_F"].append(algorithm.opt.get("F")[0][0])
         self.data["pop_F"].append(algorithm.pop.get("F"))
         self.data["n_evals"].append(algorithm.evaluator.n_eval)
 
-        if algorithm.n_iter % self.k_steps == 0:
+        df = pd.read_csv(self.csv_path)
+        # if len(df) >= 2:
+        curr_neval = df["n_eval"].to_numpy()[-1]
+        curr_niter = df["n_step"].to_numpy()[-1]
+        if self.start_iter is None:
+            self.start_iter = curr_niter
+            self.start_eval = curr_neval
+
+        # else:
+        # curr_neval = df["n_eval"].to_numpy()
+        # curr_niter = df["n_step"].to_numpy()
+
+        if (self.start_iter + algorithm.n_iter) % self.k_steps == 0:
             best_X = algorithm.opt.get("X")[0]
 
             NP = len(algorithm.pop)
             # algorithm.evaluator.n_eval += NP
 
-            df = pd.read_csv(self.csv_path)
             # Define the new row as a dictionary
             new_row = {
-                "n_step": algorithm.n_iter,
+                "n_step": self.start_iter + algorithm.n_iter,
+                "n_eval": self.start_eval + algorithm.evaluator.n_eval,
                 "f_best": algorithm.opt.get("F")[0][0],
                 "f_avg": algorithm.pop.get("F").mean(),
                 "f_std": algorithm.pop.get("F").std(),
@@ -282,6 +321,7 @@ class SOCallback(Callback):
             # Define the new row as a dictionary
             new_row = {
                 "n_step": intermediate_result.nit,
+                "n_eval": intermediate_result.nfev,
                 "f_best": best_F,
                 "f_avg": intermediate_result.population_energies.mean(),
                 "f_std": intermediate_result.population_energies.std(),
